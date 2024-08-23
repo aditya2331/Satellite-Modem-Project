1242e9065d9117c40317d3b60ae55dca76372370
AmeyaBagal <ee21b014@smail.iitm.ac.in>
2024-08-23 15:59:49 +0530

    Modified The modulation function to fixed point

--- BER_sim_fixedpoint/qpskMod1.m
+++ BER_sim_fixedpoint/qpskMod1.m
@@ -1,11 +1,11 @@
 function modulatedSig = qpskMod1(in_signal)
 
-%wordLength = 12;         
-%fractionLength = 7;
+wordLength = 12;         
+fractionLength = 9;
 
 modulatedSig = zeros(length(in_signal)/2,1);
 symbol_map = [1+1i, -1+1i, 1-1i, -1-1i] / sqrt(2);
-%symbol_map = fi(symbol_map,1,wordLength,fractionLength,'SumMode', 'SpecifyPrecision', 'SumWordLength', wordLength, 'SumFractionLength', fractionLength, 'ProductMode', 'SpecifyPrecision', 'ProductWordLength', wordLength, 'ProductFractionLength', fractionLength);
+symbol_map = fi(symbol_map,1,wordLength,fractionLength,'SumMode', 'SpecifyPrecision', 'SumWordLength', wordLength, 'SumFractionLength', fractionLength, 'ProductMode', 'SpecifyPrecision', 'ProductWordLength', wordLength, 'ProductFractionLength', fractionLength);
 
 for i = 1:2:length(in_signal)
     if in_signal(i) == 0 && in_signal(i+1) == 0
